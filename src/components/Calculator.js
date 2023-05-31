9e6cbabf0d805d6fc372a4dc12e31c3f9aae7889
katfogy <katfogy@gmail.com>
2023-05-31 23:02:46 +0100

    calculator operation done

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,62 @@
 import './Calculator.css';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="container">
-    <div className="display">0</div>
-    <div className="keys">
-      <button type="button" className="nosign">AC</button>
-      <button type="button" className="nosign">+/-</button>
-      <button type="button" className="nosign">%</button>
-      <button type="button" className="sign">+</button>
-      <button type="button" className="nosign">7</button>
-      <button type="button" className="nosign">8</button>
-      <button type="button" className="nosign">9</button>
-      <button type="button" className="sign">X</button>
-      <button type="button" className="nosign">4</button>
-      <button type="button" className="nosign">5</button>
-      <button type="button" className="nosign">6</button>
-      <button type="button" className="sign">-</button>
-      <button type="button" className="nosign">1</button>
-      <button type="button" className="nosign">2</button>
-      <button type="button" className="nosign">3</button>
-      <button type="button" className="sign">+</button>
-      <button type="button" className="span-2">0</button>
-      <button type="button" className="nosign">.</button>
-      <button type="button" className="sign">=</button>
+const Calculator = () => {
+  const initialState = {
+    total: 0,
+    next: '',
+    operation: '',
+  };
+
+  // Declaring the useState
+
+  const [state, setState] = useState(initialState);
+
+  // Array destructuring
+
+  const { total, operation, next } = state;
+
+  // This is the click function
+  const onClickHandler = (event) => {
+    const TargetValue = event.target.value;
+    const updateState = calculate(state, TargetValue);
+    setState(updateState);
+  };
+
+  return (
+    <div className="container">
+      <div className="display">
+        {total}
+        {' '}
+        {operation}
+        {' '}
+        {next}
+        {' '}
+      </div>
+      <div className="keys">
+        <button onClick={onClickHandler} type="button" value="AC" className="nosign">AC</button>
+        <button onClick={onClickHandler} type="button" value="+/-" className="nosign">+/-</button>
+        <button onClick={onClickHandler} type="button" value="%" className="nosign">%</button>
+        <button onClick={onClickHandler} type="button" value="+" className="sign">+</button>
+        <button onClick={onClickHandler} type="button" value="7" className="nosign">7</button>
+        <button onClick={onClickHandler} type="button" value="8" className="nosign">8</button>
+        <button onClick={onClickHandler} type="button" value="9" className="nosign">9</button>
+        <button onClick={onClickHandler} type="button" value="x" className="sign">X</button>
+        <button onClick={onClickHandler} type="button" value="4" className="nosign">4</button>
+        <button onClick={onClickHandler} type="button" value="5" className="nosign">5</button>
+        <button onClick={onClickHandler} type="button" value="6" className="nosign">6</button>
+        <button onClick={onClickHandler} type="button" value="-" className="sign">-</button>
+        <button onClick={onClickHandler} type="button" value="1" className="nosign">1</button>
+        <button onClick={onClickHandler} type="button" value="2" className="nosign">2</button>
+        <button onClick={onClickHandler} type="button" value="3" className="nosign">3</button>
+        <button onClick={onClickHandler} type="button" value="+" className="sign">+</button>
+        <button onClick={onClickHandler} type="button" value="0" className="span-2">0</button>
+        <button onClick={onClickHandler} type="button" value="." className="nosign">.</button>
+        <button onClick={onClickHandler} type="button" value="=" className="sign">=</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
