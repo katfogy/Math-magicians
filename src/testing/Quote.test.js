9a74240219dc14dd16b141856c6ed05037d1ccfb
katfogy <katfogy@gmail.com>
2023-06-08 20:32:01 +0100

    linters error fixed

--- src/testing/Quote.test.js
+++ src/testing/Quote.test.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import renderer from 'react-test-renderer';
-import Quotes from '../components/quotes'
+import Quotes from '../components/quotes';
 
 describe('Quotes', () => {
   it('Should render correctly', () => {
