7abdd683dacfd5ce642c6bc2baeaddab312ffb13
katfogy <katfogy@gmail.com>
2023-06-08 20:39:40 +0100

    linters error fixed

--- src/testing/Calculator.test.js
+++ src/testing/Calculator.test.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import Calculator from '../components/calculator';
+import Calculator from '../components/Calculator';
 
 it('renders correctly', () => {
   const tree = renderer
